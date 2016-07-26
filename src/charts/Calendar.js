1eb0cbae6c33a153ba6beb423d84e6c2120e3e8f
chidori <kagakinam@gmail.com>
2016-07-26 13:15:26 +0800

    work

--- src/charts/Calendar.js
+++ src/charts/Calendar.js
@@ -54,6 +54,12 @@ anychart.charts.Calendar.prototype.data = function(opt_data, opt_csvSettings) {
 
 //region --- INHERITED API ---
 /** @inheritDoc */
+anychart.charts.Calendar.prototype.getType = function() {
+  return anychart.enums.ChartTypes.CALENDAR;
+};
+
+
+/** @inheritDoc */
 anychart.charts.Calendar.prototype.getAllSeries = function() {
   return [];
 };
@@ -146,6 +152,76 @@ anychart.charts.Calendar.prototype.colorRangeInvalidated_ = function(event) {
   // if there are no signals, state == 0 and nothing happens.
   this.invalidate(state, signal);
 };
+
+
+/**
+ * Data for calendar chart.
+ * @param {?(anychart.data.View|anychart.data.Set|Array|string)=} opt_value Value to set.
+ * @param {Object.<string, (string|boolean)>=} opt_csvSettings If CSV string is passed, you can pass CSV parser settings here as a hash map.
+ * @return {(!anychart.charts.Calendar|!anychart.data.View)} Returns itself if used as a setter or the mapping if used as a getter.
+ */
+anychart.charts.Calendar.prototype.data = function(opt_value, opt_csvSettings) {
+  if (goog.isDef(opt_value)) {
+    if (this.rawData_ !== opt_value) {
+      this.rawData_ = opt_value;
+      goog.dispose(this.parentViewToDispose_);
+      this.iterator_ = null;
+      if (opt_value instanceof anychart.data.View)
+        this.parentView_ = this.parentViewToDispose_ = opt_value.derive();
+      else if (opt_value instanceof anychart.data.Set)
+        this.parentView_ = this.parentViewToDispose_ = opt_value.mapAs();
+      else
+        this.parentView_ = (this.parentViewToDispose_ = new anychart.data.Set(
+            (goog.isArray(opt_value) || goog.isString(opt_value)) ? opt_value : null, opt_csvSettings)).mapAs();
+      this.registerDisposable(this.parentViewToDispose_);
+      this.data_ = this.parentView_;
+      this.data_.listenSignals(this.dataInvalidated_, this);
+
+      this.invalidatePointers();
+      this.invalidate(anychart.ConsistencyState.GAUGE_POINTERS |
+          anychart.ConsistencyState.GAUGE_SCALE,
+          anychart.Signal.NEEDS_REDRAW |
+          anychart.Signal.NEEDS_RECALCULATION);
+    }
+
+    return this;
+  }
+  return this.data_;
+};
+
+
+/**
+ * Getter/setter for startMonth.
+ * @param {number|string=} opt_value startMonth.
+ * @return {number|string|anychart.charts.Calendar} startMonth or self for chaining.
+ */
+anychart.charts.Calendar.prototype.startMonth = function(opt_value) {
+  if (goog.isDef(opt_value)) {
+    if (this.startMonth_ != opt_value) {
+      this.startMonth_ = opt_value;
+      this.invalidate(anychart.ConsistencyState.APPEARANCE, anychart.Signal.NEEDS_REDRAW);
+    }
+    return this;
+  }
+  return this.startMonth_;
+};
+
+
+/**
+ * Getter/setter for endMonth.
+ * @param {number|string=} opt_value endMonth.
+ * @return {number|string|anychart.charts.Calendar} endMonth or self for chaining.
+ */
+anychart.charts.Calendar.prototype.endMonth = function(opt_value) {
+  if (goog.isDef(opt_value)) {
+    if (this.endMonth_ != opt_value) {
+      this.endMonth_ = opt_value;
+      this.invalidate(anychart.ConsistencyState.APPEARANCE, anychart.Signal.NEEDS_REDRAW);
+    }
+    return this;
+  }
+  return this.endMonth_;
+};
 //endregion
 
 
@@ -161,9 +237,26 @@ anychart.charts.Calendar.prototype.selectMarkers = function() {};
 
 //region --- DRAWING ---
 /** @inheritDoc */
+anychart.charts.Calendar.prototype.calculate = function() {
+  anychart.charts.Calendar.base(this, 'calculate');
+};
+
+
+/** @inheritDoc */
 anychart.charts.Calendar.prototype.drawContent = function(bounds) {
   if (this.isConsistent())
     return;
+
+  this.calculate();
+
+  if (!this.rootLayer) {
+    this.rootLayer = this.rootElement.layer();
+  }
+
+  if (this.hasInvalidationState(anychart.ConsistencyState.BOUNDS)) {
+    //
+    this.markConsistent(anychart.ConsistencyState.BOUNDS);
+  }
 
   if (this.hasInvalidationState(anychart.ConsistencyState.APPEARANCE)) {
     this.markConsistent(anychart.ConsistencyState.APPEARANCE);
@@ -176,6 +269,8 @@ anychart.charts.Calendar.prototype.drawContent = function(bounds) {
 /** @inheritDoc */
 anychart.charts.Calendar.prototype.serialize = function() {
   var json = anychart.charts.Calendar.base(this, 'serialize');
+  json['type'] = this.getType();
+  json['data'] = this.data().serialize();
 
   json['colorScale'] = this.colorScale().serialize();
   json['colorRange'] = this.colorRange().serialize();
