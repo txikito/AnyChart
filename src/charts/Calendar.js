d05340307dfab00828e65cfc0571cba05d061bfc
chidori <kagakinam@gmail.com>
2016-08-03 22:00:07 +0800

    data added + data preparing

--- src/charts/Calendar.js
+++ src/charts/Calendar.js
@@ -37,19 +37,10 @@ anychart.charts.Calendar.prototype.SUPPORTED_SIGNALS =
  */
 anychart.charts.Calendar.prototype.SUPPORTED_CONSISTENCY_STATES =
     anychart.core.SeparateChart.prototype.SUPPORTED_CONSISTENCY_STATES |
+    anychart.ConsistencyState.APPEARANCE |
     anychart.ConsistencyState.CALENDAR_COLOR_SCALE |
     anychart.ConsistencyState.CALENDAR_COLOR_RANGE;
 //endregion
-
-
-/**
- * Getter/setter data.
- * @param {(anychart.data.View|anychart.data.Set|Array|string)=} opt_data Data for the chart.
- * @param {Object.<string, (string|boolean)>=} opt_csvSettings If CSV string is passed, you can pass CSV parser settings here as a hash map.
- */
-anychart.charts.Calendar.prototype.data = function(opt_data, opt_csvSettings) {
-  //
-};
 
 
 //region --- INHERITED API ---
@@ -73,6 +64,79 @@ anychart.charts.Calendar.prototype.createLegendItemsProvider = function() {
 
 
 //region --- OWN API ---
+/**
+ * Prepares data.
+ */
+anychart.charts.Calendar.prototype.prepareData = function() {
+  goog.dispose(this.data_);
+  delete this.iterator_;
+  this.data_ = this.parentView_.sort('x', function(x1, x2) {
+    return anychart.utils.normalizeTimestamp(x1) - anychart.utils.normalizeTimestamp(x2);
+  });
+  this.data_.listenSignals(this.dataInvalidated_, this);
+
+  this.invalidate(anychart.ConsistencyState.APPEARANCE, anychart.Signal.NEEDS_REDRAW | anychart.Signal.NEEDS_RECALCULATION);
+};
+
+
+/**
+ * Getter/setter for calendar data.
+ * @param {?(anychart.data.View|anychart.data.Set|Array|string)=} opt_value Value to set.
+ * @param {Object.<string, (string|boolean)>=} opt_csvSettings If CSV string is passed, you can pass CSV parser settings here as a hash map.
+ * @return {(!anychart.charts.Calendar|!anychart.data.View)} Returns itself if used as a setter or the mapping if used as a getter.
+ */
+anychart.charts.Calendar.prototype.data = function(opt_value, opt_csvSettings) {
+  if (goog.isDef(opt_value)) {
+    if (this.rawData_ !== opt_value) {
+      this.rawData_ = opt_value;
+      goog.dispose(this.parentViewToDispose_);
+      this.iterator_ = null;
+      if (opt_value instanceof anychart.data.View)
+        this.parentView_ = this.parentViewToDispose_ = opt_value.derive();
+      else if (opt_value instanceof anychart.data.Set)
+        this.parentView_ = this.parentViewToDispose_ = opt_value.mapAs();
+      else
+        this.parentView_ = (this.parentViewToDispose_ = new anychart.data.Set(
+            (goog.isArray(opt_value) || goog.isString(opt_value)) ? opt_value : null, opt_csvSettings)).mapAs();
+      this.prepareData();
+    }
+
+    return this;
+  }
+  return this.data_;
+};
+
+
+/**
+ * Data invalidation handler.
+ * @param {anychart.SignalEvent} e
+ * @private
+ */
+anychart.charts.Calendar.prototype.dataInvalidated_ = function(e) {
+  if (e.hasSignal(anychart.Signal.DATA_CHANGED)) {
+    this.invalidate(anychart.ConsistencyState.APPEARANCE, anychart.Signal.NEEDS_REDRAW);
+  }
+};
+
+
+/**
+ * Returns current iterator.
+ * @return {!anychart.data.Iterator}
+ */
+anychart.charts.Calendar.prototype.getIterator = function() {
+  return this.iterator_ || this.getResetIterator();
+};
+
+
+/**
+ * Returns reset iterator.
+ * @return {!anychart.data.Iterator}
+ */
+anychart.charts.Calendar.prototype.getResetIterator = function() {
+  return (this.iterator_ = this.data_.getIterator());
+};
+
+
 /**
  * Color scale.
  * @param {(anychart.scales.LinearColor|anychart.scales.OrdinalColor)=} opt_value Scale to set.
@@ -155,42 +219,6 @@ anychart.charts.Calendar.prototype.colorRangeInvalidated_ = function(event) {
 
 
 /**
- * Data for calendar chart.
- * @param {?(anychart.data.View|anychart.data.Set|Array|string)=} opt_value Value to set.
- * @param {Object.<string, (string|boolean)>=} opt_csvSettings If CSV string is passed, you can pass CSV parser settings here as a hash map.
- * @return {(!anychart.charts.Calendar|!anychart.data.View)} Returns itself if used as a setter or the mapping if used as a getter.
- */
-anychart.charts.Calendar.prototype.data = function(opt_value, opt_csvSettings) {
-  if (goog.isDef(opt_value)) {
-    if (this.rawData_ !== opt_value) {
-      this.rawData_ = opt_value;
-      goog.dispose(this.parentViewToDispose_);
-      this.iterator_ = null;
-      if (opt_value instanceof anychart.data.View)
-        this.parentView_ = this.parentViewToDispose_ = opt_value.derive();
-      else if (opt_value instanceof anychart.data.Set)
-        this.parentView_ = this.parentViewToDispose_ = opt_value.mapAs();
-      else
-        this.parentView_ = (this.parentViewToDispose_ = new anychart.data.Set(
-            (goog.isArray(opt_value) || goog.isString(opt_value)) ? opt_value : null, opt_csvSettings)).mapAs();
-      this.registerDisposable(this.parentViewToDispose_);
-      this.data_ = this.parentView_;
-      this.data_.listenSignals(this.dataInvalidated_, this);
-
-      this.invalidatePointers();
-      this.invalidate(anychart.ConsistencyState.GAUGE_POINTERS |
-          anychart.ConsistencyState.GAUGE_SCALE,
-          anychart.Signal.NEEDS_REDRAW |
-          anychart.Signal.NEEDS_RECALCULATION);
-    }
-
-    return this;
-  }
-  return this.data_;
-};
-
-
-/**
  * Getter/setter for startMonth.
  * @param {number|string=} opt_value startMonth.
  * @return {number|string|anychart.charts.Calendar} startMonth or self for chaining.
@@ -222,6 +250,87 @@ anychart.charts.Calendar.prototype.endMonth = function(opt_value) {
   }
   return this.endMonth_;
 };
+
+
+/**
+ * Getter/setter for year.
+ * @param {string=} opt_value year.
+ * @return {string|anychart.charts.Calendar} year or self for chaining.
+ */
+anychart.charts.Calendar.prototype.year = function(opt_value) {
+  if (goog.isDef(opt_value)) {
+    if (this.year_ != opt_value) {
+      this.year_ = opt_value;
+      this.invalidate(anychart.ConsistencyState.APPEARANCE, anychart.Signal.NEEDS_REDRAW);
+    }
+    return this;
+  }
+  return this.year_;
+};
+
+
+/**
+ * Getter/setter for weekStart.
+ * @param {number=} opt_value weekStart.
+ * @return {number|anychart.charts.Calendar} weekStart or self for chaining.
+ */
+anychart.charts.Calendar.prototype.weekStart = function(opt_value) {
+  if (goog.isDef(opt_value)) {
+    opt_value = goog.math.clamp(anychart.utils.normalizeToNaturalNumber(opt_value, 1, true), 0, 6);
+    if (this.weekStart_ != opt_value) {
+      this.weekStart_ = opt_value;
+      this.invalidate(anychart.ConsistencyState.APPEARANCE, anychart.Signal.NEEDS_REDRAW);
+    }
+    return this;
+  }
+  return this.weekStart_;
+};
+
+
+/**
+ * Calendar outline month stroke (stroke for month with data).
+ * @param {(acgraph.vector.Stroke|acgraph.vector.ColoredFill|Function|string|null)=} opt_strokeOrFill Fill settings
+ *    or stroke settings.
+ * @param {number=} opt_thickness [1] Line thickness.
+ * @param {string=} opt_dashpattern Controls the pattern of dashes and gaps used to stroke paths.
+ * @param {acgraph.vector.StrokeLineJoin=} opt_lineJoin Line joint style.
+ * @param {acgraph.vector.StrokeLineCap=} opt_lineCap Line cap style.
+ * @return {(!anychart.charts.Calendar|acgraph.vector.Stroke|Function)} .
+ */
+anychart.charts.Calendar.prototype.outlineColor = function(opt_strokeOrFill, opt_thickness, opt_dashpattern, opt_lineJoin, opt_lineCap) {
+  if (goog.isDef(opt_strokeOrFill)) {
+    var stroke = goog.isFunction(opt_strokeOrFill) ?
+        opt_strokeOrFill :
+        acgraph.vector.normalizeStroke.apply(null, arguments);
+    if (stroke != this.outlineColor_) {
+      this.outlineColor_ = stroke;
+      this.invalidate(anychart.ConsistencyState.APPEARANCE, anychart.Signal.NEEDS_REDRAW);
+    }
+    return this;
+  }
+  return this.outlineColor_;
+};
+
+
+/**
+ * Calendar hover outline month stroke (stroke for month with data).
+ * @param {(acgraph.vector.Stroke|acgraph.vector.ColoredFill|Function|string|null)=} opt_strokeOrFill Fill settings
+ *    or stroke settings.
+ * @param {number=} opt_thickness [1] Line thickness.
+ * @param {string=} opt_dashpattern Controls the pattern of dashes and gaps used to stroke paths.
+ * @param {acgraph.vector.StrokeLineJoin=} opt_lineJoin Line joint style.
+ * @param {acgraph.vector.StrokeLineCap=} opt_lineCap Line cap style.
+ * @return {(!anychart.charts.Calendar|acgraph.vector.Stroke|Function)} .
+ */
+anychart.charts.Calendar.prototype.hoverOutlineColor = function(opt_strokeOrFill, opt_thickness, opt_dashpattern, opt_lineJoin, opt_lineCap) {
+  if (goog.isDef(opt_strokeOrFill)) {
+    this.hoverOutlineColor_ = goog.isFunction(opt_strokeOrFill) ?
+        opt_strokeOrFill :
+        acgraph.vector.normalizeStroke.apply(null, arguments);
+    return this;
+  }
+  return this.hoverOutlineColor_;
+};
 //endregion
 
 
@@ -239,6 +348,21 @@ anychart.charts.Calendar.prototype.selectMarkers = function() {};
 /** @inheritDoc */
 anychart.charts.Calendar.prototype.calculate = function() {
   anychart.charts.Calendar.base(this, 'calculate');
+
+  if (this.hasInvalidationState(anychart.ConsistencyState.CALENDAR_COLOR_SCALE)) {
+    var scale = this.colorScale();
+    if (scale && scale.needsAutoCalc()) {
+      scale.startAutoCalc();
+
+      var iterator = this.getIterator();
+      while (iterator.advance()) {
+        scale.extendDataRange(iterator.get('value'));
+      }
+
+      scale.finishAutoCalc();
+    }
+    this.markConsistent(anychart.ConsistencyState.CALENDAR_COLOR_SCALE);
+  }
 };
 
 
@@ -258,7 +382,15 @@ anychart.charts.Calendar.prototype.drawContent = function(bounds) {
     this.markConsistent(anychart.ConsistencyState.BOUNDS);
   }
 
+  var weekStart, startMonth, endMonth;
+  var iterator = this.getIterator();
   if (this.hasInvalidationState(anychart.ConsistencyState.APPEARANCE)) {
+    while (iterator.advance()) {
+      console.log(iterator.get('x'), iterator.get('value'));
+    }
+
+
+
     this.markConsistent(anychart.ConsistencyState.APPEARANCE);
   }
 };
@@ -304,6 +436,8 @@ anychart.charts.Calendar.prototype.setupByJSON = function(config) {
 
 /** @inheritDoc */
 anychart.charts.Calendar.prototype.disposeInternal = function() {
+  goog.dispose(this.rootLayer);
+  goog.dispose(this.parentViewToDispose_);
   anychart.charts.Calendar.base(this, 'disposeInternal');
 };
 //endregion
