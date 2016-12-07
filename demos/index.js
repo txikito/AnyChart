fa012509e033f4e91fdfba5e18ab0e0101996e64
blackart <blackart.ru@gmail.com>
2016-12-07 17:56:56 +0800

    nothing

--- demos/index.js
+++ demos/index.js
@@ -1,5 +1,6 @@
 anychart.onDocumentReady(function() {
   chart = anychart.column([1, 2, 3, 4, 5]);
+  chart.title('canvg is a SVG parser and renderer. It takes a URL to a SVG file or the text of an SVG file, parses it in JavaScript, and renders the result on a Canvas element. The rendering speed of the examples is about as fast as native SVG.')
   chart.bounds(0, 0, 800, 600);
   chart.container('container').draw();
 });
