1a135c28295fb21e94ce964abbdada45dac70675
Alexander Kudryavtsev <clear.meltdown@gmail.com>
2017-05-30 11:33:51 +0800

    Chart disposing with tooltip

--- src/core/Chart.js
+++ src/core/Chart.js
@@ -1828,13 +1828,12 @@ anychart.core.Chart.prototype.setupByJSON = function(config, opt_default) {
 
 /** @inheritDoc */
 anychart.core.Chart.prototype.disposeInternal = function() {
-  goog.disposeAll(this.animation_, this.a11y_);
-  anychart.core.Chart.base(this, 'disposeInternal');
-
   goog.disposeAll(this.animation_, this.a11y_, this.tooltip_);
   this.animation_ = null;
   this.a11y_ = null;
   this.tooltip_ = null;
+
+  anychart.core.Chart.base(this, 'disposeInternal');
 
   if (this.id_)
     anychart.untrackChart(this, /** @type {string} */(this.id_));
