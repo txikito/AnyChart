ddd54e9fa35874ffb1b3f8954223347ea76e81f3
chernetsky <chernetsky@gmail.com>
2017-07-03 15:34:05 +0800

    series.createPositionProvider fix

--- src/core/series/Base.js
+++ src/core/series/Base.js
@@ -3756,6 +3756,8 @@ anychart.core.series.Base.prototype.createPositionProvider = function(position, 
       point = this.createPositionProviderByGeometry(/** @type {anychart.enums.Anchor} */(anchor));
     } else {
       point = this.createPositionProviderByData(position);
+      if (isNaN(point['x']) || isNaN(point['y']))
+        point = this.createPositionProviderByGeometry(anychart.enums.Anchor.CENTER_TOP);
     }
 
     if (opt_shift3D && this.check(anychart.core.drawers.Capabilities.IS_3D_BASED)) {
