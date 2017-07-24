3019f1909ef304b1daecbdc0df974030de56ec6b
chernetsky <chernetsky@gmail.com>
2017-07-24 16:48:48 +0800

    in progress

---
+++ src/chartEditor2/chartEditor2.module.js
@@ -0,0 +1,7 @@
+/**
+ * @fileoverview anychart.modules.chart_editor2 namespace file.
+ * @suppress {extraRequire}
+ */
+
+goog.provide('anychart.chartEditor2Module.entry');
+goog.require('anychart.chartEditor2Module.Editor');
