7e70b224b98517183f5f6d8e87c6d1fdc2e02ba2
Roman Lubushkin <ra.lubushkin@gmail.com>
2017-08-11 15:55:39 +0800

    Pull from develop

--- src/deps.js
+++ src/deps.js
@@ -28,7 +28,7 @@ goog.addDependency('../../../../src/annotations/Triangle.js', ['anychart.annotat
 goog.addDependency('../../../../src/annotations/VerticalLine.js', ['anychart.annotationsModule.VerticalLine'], ['anychart.annotationsModule', 'anychart.annotationsModule.Base', 'anychart.core.settings', 'anychart.enums'], false);
 goog.addDependency('../../../../src/annotations/annotations.js', ['anychart.annotationsModule'], ['anychart.core.Base', 'anychart.core.settings', 'anychart.enums'], false);
 goog.addDependency('../../../../src/annotations/annotations.module.js', ['anychart.annotationsModule.entry'], ['anychart.annotationsModule.ChartController', 'anychart.annotationsModule.PlotController'], false);
-goog.addDependency('../../../../src/annotations/defaultTheme.js', ['anychart.annotationsModule.defaultTheme'], [], false);
+goog.addDependency('../../../../src/annotations/defaultTheme.js', ['anychart.annotationsModule.defaultTheme'], ['anychart.core.defaultTheme'], false);
 goog.addDependency('../../../../src/anychart.js', ['anychart', 'anychart.gauges', 'anychart.globalLock'], ['acgraph', 'anychart.base', 'anychart.core.reporting', 'anychart.core.ui.StageCredits', 'anychart.performance', 'anychart.themes.merging', 'anychart.utils', 'goog.array', 'goog.dom', 'goog.dom.animationFrame.polyfill', 'goog.events.KeyEvent', 'goog.events.KeyHandler', 'goog.json.hybrid'], false);
 goog.addDependency('../../../../src/base.js', ['anychart.base'], ['goog.labs.userAgent.device'], false);
 goog.addDependency('../../../../src/binding/binding.module.js', ['anychart.bindingModule.entry'], ['goog.dom', 'goog.dom.classlist', 'goog.dom.forms', 'goog.events'], false);
