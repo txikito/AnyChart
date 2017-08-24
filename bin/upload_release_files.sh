0045c44a4cffe9e461adc58336de5e571ef57113
Roman Lubushkin <ra.lubushkin@gmail.com>
2017-08-24 13:49:35 +0800

    Add dist files and CI upload rules

---
+++ bin/upload_release_files.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ev
+#if [ "${TRAVIS_BRANCH}" = "DVF-3234-minor-build-fixes" ]; then
+if [ "DVF-3234-minor-build-fixes" = "DVF-3234-minor-build-fixes" ]; then
+
+    # copy bin files
+    cp ./bin/binaries_wrapper_end.txt ./dist/binaries_wrapper_end.txt
+    cp ./bin/binaries_wrapper_start.txt ./dist/binaries_wrapper_start.txt
+
+    # zip files
+    cd ./out/
+    zip -r upload.zip *
+
+    # create release branch
+    ssh $STATIC_HOST_SSH_STRING "mkdir -p /apps/static/cdn/releases/$TRAVIS_BRANCH"
+    scp upload.zip $STATIC_HOST_SSH_STRING:/apps/static/cdn/releases/$TRAVIS_BRANCH/upload.zip
+    ssh $STATIC_HOST_SSH_STRING "unzip -o /apps/static/cdn/releases/$TRAVIS_BRANCH/upload.zip -d /apps/static/cdn/releases/$TRAVIS_BRANCH/"
+
+fi
