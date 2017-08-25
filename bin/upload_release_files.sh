ad084556d1f5220293e7891021b1b01fcd6bdc62
Roman Lubushkin <ra.lubushkin@gmail.com>
2017-08-25 12:23:17 +0800

    Copy release files to legacy folders and latest

--- bin/upload_release_files.sh
+++ bin/upload_release_files.sh
@@ -19,11 +19,45 @@ if [ "${TRAVIS_BRANCH}" = "DVF-3234-minor-build-fixes" ]; then
     rm demos.zip
 
     # zip files
-    zip -r upload.zip *
+    zip -r installation-package.zip *
 
-    # create release branch
-    ssh $STATIC_HOST_SSH_STRING "mkdir -p /apps/static/cdn/releases/$TRAVIS_BRANCH"
-    scp upload.zip $STATIC_HOST_SSH_STRING:/apps/static/cdn/releases/$TRAVIS_BRANCH/upload.zip
-    ssh $STATIC_HOST_SSH_STRING "unzip -o /apps/static/cdn/releases/$TRAVIS_BRANCH/upload.zip -d /apps/static/cdn/releases/$TRAVIS_BRANCH/ && rm /apps/static/cdn/releases/$TRAVIS_BRANCH/upload.zip"
+    # ensure releases branch exists and clean, as far as cdn always hosts removed content, we are free to remove files
+    ssh $STATIC_HOST_SSH_STRING "
+    mkdir -p /apps/static/cdn/releases/$TRAVIS_BRANCH &&
+    rm -rf /apps/static/cdn/releases/$TRAVIS_BRANCH/* &&
+    mkdir -p /apps/static/cdn/releases/latest &&
+    rm -rf /apps/static/cdn/releases/latest/*"
+
+    # upload content
+    scp installation-package.zip $STATIC_HOST_SSH_STRING:/apps/static/cdn/releases/$TRAVIS_BRANCH/installation-package.zip
+
+    # copy unzip release files and copy to latest
+    ssh $STATIC_HOST_SSH_STRING "
+    unzip -o /apps/static/cdn/releases/$TRAVIS_BRANCH/installation-package.zip -d /apps/static/cdn/releases/$TRAVIS_BRANCH/ &&
+    cp -r /apps/static/cdn/releases/$TRAVIS_BRANCH /apps/static/cdn/releases/latest"
+
+    # copy legacy files by version and latest
+    ssh $STATIC_HOST_SSH_STRING "
+    cp -r /apps/static/cdn/releases/$TRAVIS_BRANCH/js /apps/static/cdn/js/$TRAVIS_BRANCH &&
+    cp -r /apps/static/cdn/releases/$TRAVIS_BRANCH/js /apps/static/cdn/js/latest &&
+    cp -r /apps/static/cdn/releases/$TRAVIS_BRANCH/css /apps/static/cdn/css/$TRAVIS_BRANCH &&
+    cp -r /apps/static/cdn/releases/$TRAVIS_BRANCH/css /apps/static/cdn/css/latest &&
+    cp -r /apps/static/cdn/releases/$TRAVIS_BRANCH/themes /apps/static/cdn/themes/$TRAVIS_BRANCH &&
+    cp -r /apps/static/cdn/releases/$TRAVIS_BRANCH/themes /apps/static/cdn/themes/latest &&
+    mkdir -p /apps/static/cdn/schemas/$TRAVIS_BRANCH &&
+    cp /apps/static/cdn/releases/$TRAVIS_BRANCH/json-schema.json /apps/static/cdn/schemas/$TRAVIS_BRANCH/json-schema.json &&
+    cp /apps/static/cdn/releases/$TRAVIS_BRANCH/json-schema.json /apps/static/cdn/schemas/latest/json-schema.json &&
+    cp /apps/static/cdn/releases/$TRAVIS_BRANCH/xml-schema.xsd /apps/static/cdn/schemas/$TRAVIS_BRANCH/xml-schema.xsd &&
+    cp /apps/static/cdn/releases/$TRAVIS_BRANCH/xml-schema.xsd /apps/static/cdn/schemas/latest/xml-schema.xsd"
 
 fi
+
+
+# todo
+# 3. drop cdn cache for releases
+# 4. drop cdn cache for legacy files
+# 5. create github tag
+# 6. upload github release
+# 7. make npm publish
+# 8. version regexp or develop
+# 9. build export server ???
