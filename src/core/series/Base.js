2d79921755f8932be9f70eca93aff29a163bf028
Alexander Kudryavtsev <clear.meltdown@gmail.com>
2017-09-20 16:33:16 +0800

    QA fix

--- src/core/series/Base.js
+++ src/core/series/Base.js
@@ -258,7 +258,7 @@ anychart.core.series.Base = function(chart, plot, type, config) {
       anychart.core.drawers.Capabilities.IS_WIDTH_BASED],
     ['minPointLength',
       anychart.ConsistencyState.SERIES_POINTS,
-      anychart.Signal.NEEDS_REDRAW,
+      anychart.Signal.NEEDS_REDRAW | anychart.Signal.NEEDS_RECALCULATION,
       anychart.core.drawers.Capabilities.IS_WIDTH_BASED,
       this.resetSharedStack
     ],
@@ -3486,11 +3486,19 @@ anychart.core.series.Base.prototype.makeMinPointLengthStackedMeta = function(row
 
     var y = /** @type {number} */ (rowInfo.meta('value'));
     var zero = /** @type {number} */ (rowInfo.meta('zero'));
+    var isZero = /** @type {number} */ (rowInfo.get('value')) == 0; //Draw zero to positive side.
     var diff = Math.abs(y - zero);
     var height = Math.max(diff, this.minPointLengthCache_);
 
     var newZero, newY;
-    if (zero >= y) {
+    var positive = zero >= y;
+    if (isZero) {
+      var isVertical = /** @type {boolean} */ (this.getOption('isVertical'));
+      var inverted = this.yScale().inverted();
+      positive = !(isVertical ^ inverted);
+    }
+
+    if (positive) {
       height = -height;
       if (isNaN(shared.positiveAnchor)) {//Drawing first point.
         shared.positiveAnchor = zero + height;
@@ -3534,11 +3542,46 @@ anychart.core.series.Base.prototype.makeMinPointLengthUnstackedMeta = function(r
   if (!rowInfo.meta('missing')) {
     var y = /** @type {number} */ (rowInfo.meta('value'));
     var zero = /** @type {number} */ (rowInfo.meta('zero'));
+    var isZero = /** @type {number} */ (rowInfo.get('value')) == 0; //Draw zero to positive side.
     var diff = Math.abs(y - zero);
     var height = Math.max(diff, this.minPointLengthCache_);
-    if (zero >= y)
+
+    var positive = zero >= y;
+    if (isZero) {
+      var isVertical = /** @type {boolean} */ (this.getOption('isVertical'));
+      var inverted = this.yScale().inverted();
+      positive = !(isVertical ^ inverted);
+    }
+
+    if (positive)
       height = -height;
     rowInfo.meta('value', zero + height);
+  }
+  return pointMissing;
+};
+
+
+/**
+ * Applies min point length settings for ranged plan.
+ * @param {anychart.data.IRowInfo} rowInfo
+ * @param {Array.<string>} yNames
+ * @param {Array.<string|number>} yColumns
+ * @param {number} pointMissing
+ * @param {number} xRatio
+ * @return {number} - pointMissing updated value.
+ * @protected
+ */
+anychart.core.series.Base.prototype.makeMinPointLengthRangedMeta = function(rowInfo, yNames, yColumns, pointMissing, xRatio) {
+  if (!rowInfo.meta('missing')) {
+    var high = /** @type {number} */(rowInfo.meta('high'));
+    var low = /** @type {number} */(rowInfo.meta('low'));
+    var center = (high + low) / 2;
+    var diff = Math.abs(low - high);
+    var height = Math.max(diff, this.minPointLengthCache_) / 2;
+    if (low >= high)
+      height = -height;
+    rowInfo.meta('high', center - height);
+    rowInfo.meta('low', center + height);
   }
   return pointMissing;
 };
@@ -3682,7 +3725,11 @@ anychart.core.series.Base.prototype.prepareMetaMakers = function(yNames, yColumn
     var isVertical = /** @type {boolean} */ (this.getOption('isVertical'));
     var dimension = isVertical ? this.pixelBoundsCache.width : this.pixelBoundsCache.height;
     this.minPointLengthCache_ = Math.abs(anychart.utils.normalizeSize(minPointLength, dimension));
-    if (this.planIsStacked()) {
+
+    var isRangeSeries = this.check(anychart.core.drawers.Capabilities.IS_RANGE_BASED | anychart.core.drawers.Capabilities.IS_OHLC_BASED);
+    if (isRangeSeries) {
+      this.metaMakers.push(this.makeMinPointLengthRangedMeta);
+    } else if (this.planIsStacked()) {
       this.metaMakers.push(this.makeMinPointLengthStackedMeta);
     } else {
       this.metaMakers.push(this.makeMinPointLengthUnstackedMeta);
