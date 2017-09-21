b77f9023a0b9311097bb81d377ae2a93a38845d0
Alexander Kudryavtsev <clear.meltdown@gmail.com>
2017-09-21 11:04:56 +0800

    Min Point length fix

--- src/core/ChartWithSeries.js
+++ src/core/ChartWithSeries.js
@@ -1109,9 +1109,7 @@ anychart.core.ChartWithSeries.prototype.drawSeries = function(opt_topAxisPadding
     }
 
     this.beforeSeriesDraw();
-    for (i = 0; i < this.seriesList.length; i++) {
-      this.seriesList[i].draw();
-    }
+    this.drawSeriesInOrder();
     this.afterSeriesDraw();
     this.resetSeriesStack(true);
 
@@ -1119,6 +1117,17 @@ anychart.core.ChartWithSeries.prototype.drawSeries = function(opt_topAxisPadding
     anychart.core.Base.resumeSignalsDispatchingFalse(this.seriesList);
   }
   anychart.performance.end('Series drawing');
+};
+
+
+/**
+ * Draws series in correct order.
+ * @protected
+ */
+anychart.core.ChartWithSeries.prototype.drawSeriesInOrder = function() {
+  for (var i = 0; i < this.seriesList.length; i++) {
+    this.seriesList[i].draw();
+  }
 };
 
 
