220422f730ded4ab682abef0691945a147d84be8
Roman Lubushkin <ra.lubushkin@gmail.com>
2017-09-22 12:38:37 +0800

    Fix release script version issues

--- bin/upload_release_files.sh
+++ bin/upload_release_files.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-if [ "${TRAVIS_BRANCH}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ || "${TRAVIS_BRANCH}" = "DVF-3234-minor-build-fixes" ]; then
+
+#TRAVIS_BRANCH=DVF-3234-minor-build-fixes
+GALLERY_VERSION=RC-8.0.0
+VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+
+echo ${VERSION}
+echo ${GALLERY_VERSION}
+echo ${TRAVIS_BRANCH}
+
+if [ "${TRAVIS_BRANCH}" = "master" ] ||  [ "${TRAVIS_BRANCH}" = "DVF-3234-minor-build-fixes" ]; then
 
     # copy bin files
     cp ./bin/binaries_wrapper_end.txt ./dist/binaries_wrapper_end.txt
@@ -14,42 +23,38 @@ if [ "${TRAVIS_BRANCH}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ || "${TRAVIS_BRANCH}" = "DVF
     rm docs.zip
 
     # download demos
-    wget https://playground.anychart.com/gallery/7.14.3/download -O demos.zip
+    wget https://playground.anychart.com/gallery/${GALLERY_VERSION}/download -O demos.zip
     unzip -q demos.zip -d demos
     rm demos.zip
 
     # zip files
     zip -q -r installation-package.zip *
 
-    # ensure releases branch exists and clean, as far as cdn always hosts removed content, we are free to remove files
+    # ensure release path exists and clean
+    # as far as cdn always hosts removed content, we are free to clean entire folder
     ssh $STATIC_HOST_SSH_STRING "
-    mkdir -p /apps/static/cdn/releases/$TRAVIS_BRANCH &&
-    rm -rf /apps/static/cdn/releases/$TRAVIS_BRANCH/* &&
-    mkdir -p /apps/static/cdn/releases/latest &&
+    mkdir -p /apps/static/cdn/releases/${VERSION} &&
+    rm -rf /apps/static/cdn/releases/${VERSION}/* &&
+    mkdir -p /apps/static/cdn/releases &&
     rm -rf /apps/static/cdn/releases/latest"
 
     # upload content
-    scp installation-package.zip $STATIC_HOST_SSH_STRING:/apps/static/cdn/releases/$TRAVIS_BRANCH/installation-package.zip
+    scp installation-package.zip $STATIC_HOST_SSH_STRING:/apps/static/cdn/releases/${VERSION}/installation-package.zip
 
     # copy unzip release files and copy to latest
     ssh $STATIC_HOST_SSH_STRING "
-    unzip -q -o /apps/static/cdn/releases/$TRAVIS_BRANCH/installation-package.zip -d /apps/static/cdn/releases/$TRAVIS_BRANCH/ &&
-    cp -r /apps/static/cdn/releases/$TRAVIS_BRANCH /apps/static/cdn/releases/latest"
+    unzip -q -o /apps/static/cdn/releases/${VERSION}/installation-package.zip -d /apps/static/cdn/releases/${VERSION}/ &&
+    cp -r /apps/static/cdn/releases/${VERSION} /apps/static/cdn/releases/latest"
 
     # copy legacy files by version and latest
     ssh $STATIC_HOST_SSH_STRING "
-    cp -r /apps/static/cdn/releases/$TRAVIS_BRANCH/js /apps/static/cdn/js/$TRAVIS_BRANCH &&
-    cp -r /apps/static/cdn/releases/$TRAVIS_BRANCH/css /apps/static/cdn/css/$TRAVIS_BRANCH &&
-    cp -r /apps/static/cdn/releases/$TRAVIS_BRANCH/themes /apps/static/cdn/themes/$TRAVIS_BRANCH &&
-    mkdir -p /apps/static/cdn/schemas/$TRAVIS_BRANCH &&
-    cp /apps/static/cdn/releases/$TRAVIS_BRANCH/json-schema.json /apps/static/cdn/schemas/$TRAVIS_BRANCH/json-schema.json &&
-    cp /apps/static/cdn/releases/$TRAVIS_BRANCH/xml-schema.xsd /apps/static/cdn/schemas/$TRAVIS_BRANCH/xml-schema.xsd"
+    cp -r /apps/static/cdn/releases/${VERSION}/js /apps/static/cdn/js/${VERSION} &&
+    cp -r /apps/static/cdn/releases/${VERSION}/css /apps/static/cdn/css/${VERSION} &&
+    cp -r /apps/static/cdn/releases/${VERSION}/themes /apps/static/cdn/themes/${VERSION} &&
+    mkdir -p /apps/static/cdn/schemas/${VERSION} &&
+    cp /apps/static/cdn/releases/${VERSION}/json-schema.json /apps/static/cdn/schemas/${VERSION}/json-schema.json &&
+    cp /apps/static/cdn/releases/${VERSION}/xml-schema.xsd /apps/static/cdn/schemas/${VERSION}/xml-schema.xsd"
 
-<<<<<<< HEAD
-=======
-    echo $ANYCHART_VERSION >> ./test.txt
-
->>>>>>> c7e1df9df4ce4422b3302a16b8e08721d3f11397
     #cd ../
     #./bin/drop_cdn_cache.py $TRAVIS_BRANCH $CDN_ALIASE $CDN_CONSUMER_KEY $CDN_CONSUMER_SECRET $CDN_ZONE_ID
     #./bin/upload_github_release.py $GITHUB_ACCESS_TOKEN
